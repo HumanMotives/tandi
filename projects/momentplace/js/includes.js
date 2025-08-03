9d73f2f46bb780d472a12f226248d879f8350924
Transients & Incantations <chrisvanderlinden80@gmail.com>
2025-08-03 13:39:49 +0200

    Update includes.js

--- projects/momentplace/js/includes.js
+++ projects/momentplace/js/includes.js
@@ -1,7 +1,13 @@
+// js/includes.js
 document.addEventListener('DOMContentLoaded', () => {
-  document.querySelectorAll('[data-include]').forEach(el => {
+  const includes = Array.from(document.querySelectorAll('[data-include]'));
+  const promises = includes.map(el =>
     fetch(el.getAttribute('data-include'))
       .then(res => res.text())
-      .then(html => { el.innerHTML = html });
+      .then(html => { el.innerHTML = html; })
+  );
+  // Once *all* partials are in, fire a flag event
+  Promise.all(promises).then(() => {
+    document.dispatchEvent(new Event('includesLoaded'));
   });
 });
