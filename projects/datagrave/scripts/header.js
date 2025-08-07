4795e1dc17df6602bbd99ad0da5cb5e4668a09f6
Transients & Incantations <chrisvanderlinden80@gmail.com>
2025-08-07 17:10:34 +0200

    Create header.js

---
+++ projects/datagrave/scripts/header.js
@@ -0,0 +1,9 @@
+// Load header partial into every page
+document.addEventListener('DOMContentLoaded', () => {
+  fetch('/partials/header.html')
+    .then(res => res.text())
+    .then(html => {
+      document.getElementById('common-header').innerHTML = html;
+    })
+    .catch(console.error);
+});
