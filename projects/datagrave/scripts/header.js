41a1cc7f36d27eaab25179dfcc6de4cc82fa6d19
Transients & Incantations <chrisvanderlinden80@gmail.com>
2025-08-07 19:35:12 +0200

    Update header.js

--- projects/datagrave/scripts/header.js
+++ projects/datagrave/scripts/header.js
@@ -1,6 +1,6 @@
 // Load header partial into every page
 document.addEventListener('DOMContentLoaded', () => {
-  fetch('/partials/header.html')
+  fetch('partials/header.html')
     .then(res => res.text())
     .then(html => {
       document.getElementById('common-header').innerHTML = html;
