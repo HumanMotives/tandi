d3b568ef4f29d39b2d2ad9eee7d17bbcbe5374d6
Transients & Incantations <chrisvanderlinden80@gmail.com>
2026-02-03 22:42:08 +0100

    Update splash.js

--- drummergirl/ui/splash.js
+++ drummergirl/ui/splash.js
@@ -1,27 +1,51 @@
-export function mountSplash({ logoSrc = "/assets/img/logo.png", durationMs = 5000 } = {}) {
-  const el = document.createElement("div");
-  el.className = "splash";
-  el.innerHTML = `
+// ui/splash.js
+export function mountSplash({ logoSrc = "./assets/img/logo.png", durationMs = 4000 } = {}) {
+  const root = document.createElement("div");
+  root.className = "splashScreen";
+
+  root.innerHTML = `
     <div class="splashInner">
-      <img class="splashLogo" src="${logoSrc}" alt="Drummer Girl" />
-      <div class="loadingTrack" aria-label="Loading">
-        <div class="loadingBar"></div>
+      <img class="splashLogo" src="${escapeAttr(logoSrc)}" alt="Drum School" />
+      <div class="splashBar">
+        <div class="splashBarFill"></div>
       </div>
-      <div class="loadingText">Loading drums…</div>
+      <div class="splashHint">Loading the groove…</div>
     </div>
   `;
 
-  document.body.appendChild(el);
+  document.body.appendChild(root);
+
+  const fill = root.querySelector(".splashBarFill");
+  // animate progress fill exactly durationMs
+  requestAnimationFrame(() => {
+    fill.style.transitionDuration = `${durationMs}ms`;
+    fill.style.width = "100%";
+  });
+
+  let doneResolve;
+  const donePromise = new Promise((res) => (doneResolve = res));
+
+  const timer = setTimeout(() => {
+    doneResolve();
+  }, durationMs);
 
   function destroy() {
-    el.remove();
+    clearTimeout(timer);
+    root.remove();
   }
 
   function waitDone() {
-    return new Promise((resolve) => {
-      setTimeout(() => resolve(), durationMs);
-    });
+    return donePromise;
   }
 
-  return { destroy, waitDone };
+  return { waitDone, destroy };
+}
+
+function escapeAttr(str) {
+  return String(str ?? "")
+    .replaceAll("&", "&amp;")
+    .replaceAll("<", "&lt;")
+    .replaceAll(">", "&gt;")
+    .replaceAll('"', "&quot;")
+    .replaceAll("'", "&#039;");
 }
