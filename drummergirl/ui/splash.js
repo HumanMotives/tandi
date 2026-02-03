7f4dbd24681332ea9039ad995bbd310c4400c686
Transients & Incantations <chrisvanderlinden80@gmail.com>
2026-02-03 13:18:51 +0100

    Update splash.js

--- drummergirl/ui/splash.js
+++ drummergirl/ui/splash.js
@@ -1,1 +1,27 @@
+export function mountSplash({ logoSrc = "/assets/img/logo.png", durationMs = 5000 } = {}) {
+  const el = document.createElement("div");
+  el.className = "splash";
+  el.innerHTML = `
+    <div class="splashInner">
+      <img class="splashLogo" src="${logoSrc}" alt="Drummer Girl" />
+      <div class="loadingTrack" aria-label="Loading">
+        <div class="loadingBar"></div>
+      </div>
+      <div class="loadingText">Loading drums…</div>
+    </div>
+  `;
 
+  document.body.appendChild(el);
+
+  function destroy() {
+    el.remove();
+  }
+
+  function waitDone() {
+    return new Promise((resolve) => {
+      setTimeout(() => resolve(), durationMs);
+    });
+  }
+
+  return { destroy, waitDone };
+}
