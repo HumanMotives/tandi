1381e1a3a71d7f62d618ec53e81424ae808fe8ef
Transients & Incantations <chrisvanderlinden80@gmail.com>
2026-02-03 22:09:31 +0100

    Create worlds.js

---
+++ drummergirl/data/worlds.js
@@ -0,0 +1,38 @@
+// data/worlds.js
+export const WORLDS = [
+  {
+    id: "world1",
+    titleSmall: "Wereld 1",
+    titleBig: "Eerste Stapjes",
+    thumb: "./assets/img/world_1.png",
+    requiredStarsToUnlock: 0
+  },
+  {
+    id: "world2",
+    titleSmall: "Wereld 2",
+    titleBig: "Voel de Vibe",
+    thumb: "./assets/img/world_2.png",
+    requiredStarsToUnlock: 6
+  },
+  {
+    id: "world3",
+    titleSmall: "Wereld 3",
+    titleBig: "Groovy",
+    thumb: "./assets/img/world_3.png",
+    requiredStarsToUnlock: 14
+  },
+  {
+    id: "world4",
+    titleSmall: "Wereld 4",
+    titleBig: "Showtime",
+    thumb: "./assets/img/world_4.png",
+    requiredStarsToUnlock: 24
+  },
+  {
+    id: "world5",
+    titleSmall: "Wereld 5",
+    titleBig: "Galaxy Star",
+    thumb: "./assets/img/world_5.png",
+    requiredStarsToUnlock: 36
+  }
+];
